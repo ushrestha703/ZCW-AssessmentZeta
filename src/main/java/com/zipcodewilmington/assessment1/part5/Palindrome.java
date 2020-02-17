1fa935c465a43937ed12067a222ebf86b00f9665
Ujjwal Shrestha <ushrestha@zipcoders-MBP-5.localdomain>
2020-02-17 16:30:29 -0500

    leastcommon done

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -4,8 +4,37 @@ public class Palindrome {
 
     public Integer countPalindromes(String input) {
 
+        int N = input.length();
 
-        return null;
+        // create a 2D array to store the count
+        // of palindromic subsequence
+        int[][] cps = new int[N+1][N+1];
+
+        // palindromic subsequence of length 1
+        for (int i = 0; i < N; i++)
+            cps[i][i] = 1;
+
+        // check subsequence of length L is
+        // palindrome or not
+        for (int L=2; L<=N; L++)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                int k = L + i - 1;
+                if (k < N){
+                    if (input.charAt(i) == input.charAt(k))
+                        cps[i][k] = cps[i][k-1] +
+                                cps[i+1][k] + 1;
+                    else
+                        cps[i][k] = cps[i][k-1] +
+                                cps[i+1][k] -
+                                cps[i+1][k-1];
+                }
+            }
+        }
+
+        // return total palindromic subsequence
+        return cps[0][N-1];
 
     }
 }
