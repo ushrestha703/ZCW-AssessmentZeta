9c4050bfe7872e9b8d56573ce14bcbab70c52708
Ujjwal Shrestha <ushrestha@zipcoders-MBP-5.localdomain>
2020-02-17 12:05:52 -0500

    number of occurance done. Test passed.

--- src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part2/ArrayUtils.java
@@ -11,7 +11,14 @@ public class ArrayUtils {
      * Given an array of objects, named `objectArray`, and an object `objectToCount`, return the number of times the `objectToCount` appears in the `objectArray`
      */
     public static Integer getNumberOfOccurrences(Object[] objectArray, Object objectToCount) {
-        return null;
+
+        int count = 0;
+        for (int i = 0; i < objectArray.length; i++) {
+            if (objectArray[i] == objectToCount) {
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
@@ -21,6 +28,7 @@ public class ArrayUtils {
      * Given an array of objects, name `objectArray`, and an object `objectToRemove`, return an array of objects with identical contents excluding `objectToRemove`
      */
     public static Object[] removeValue(Object[] objectArray, Object objectToRemove) {
+
         return null;
     }
 
--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -2,7 +2,11 @@ package com.zipcodewilmington.assessment1.part5;
 
 public class Palindrome {
 
-    public Integer countPalindromes(String input){
+    public Integer countPalindromes(String input) {
+
+
         return null;
+
     }
 }
+
